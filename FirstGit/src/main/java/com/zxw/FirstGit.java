66e4b68e544e2d7be5ad2b4403fc4a47522ae8c2
Aether <Aether.qq.com>
2020-03-17 16:48:41 +0800

    修改工程

--- FirstGit/src/main/java/com/zxw/FirstGit.java
+++ FirstGit/src/main/java/com/zxw/FirstGit.java
@@ -3,5 +3,6 @@ package com.zxw;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello world");
     }
 }
